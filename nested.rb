d94b8e50baeb9da8536829e148c1f87d892254f1
Josiah  Partridge <jpartridj94@gmail.com>
2020-08-25 03:37:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  returned_string = programmer_hash[:programmer_hash][:dennis_ritchie][:languages].to_s
-  returned_string
+  return = programmer_hash[:programmer_hash][:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
